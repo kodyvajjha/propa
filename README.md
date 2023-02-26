5bac452548fb0d36f4366cb8a072cae3e53a6d71
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2023-02-26 08:14:05 -0800

    feat: add a README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Parameterized Parser in Menhir
+
+This is a simple proof-of-concept as I could not find an example of a parameterized parser that was not a calculator. 
+
+This example implements a parser for the `formula` datatype in John Harrison's Handbook of Practical Logic and Automated Reasoning.  
--- bin/main.ml
+++ bin/main.ml
@@ -2,17 +2,14 @@ open Propa
 
 let () = Printexc.record_backtrace true
 
-module StringAtoms = struct
-  type t = string
-
-  let inject (t : t) : string = t
-end
-
-module StringParser = Parser.Make (StringAtoms)
-
-let str = " (vihasi_up ==> vihasi_gets_coffee)\n"
+let str = " (p /\\ q \\/ r)\n"
 
 let () =
+  let module StringParser = Parser.Make (struct
+    type t = string
+
+    let inject t = t
+  end) in
   let lexbuf = Lexing.from_string str in
   let fmla = StringParser.main Lexer.token lexbuf in
   Formula.pp CCFormat.pp_print_string Format.std_formatter fmla
